89d83d53b6e70c6d82f39ce326728546a5beb67f
Agzdjy <agzdjyxy@gmail.com>
2016-12-13 16:16:44 +0800

    更换线上数据

---
+++ generator.js
@@ -0,0 +1,47 @@
+/*global module, require*/
+
+var logger = require('./logger');
+// var util_sleep = require('../util/sleep');
+var querys = require('../query');
+
+module.exports = {
+
+  /**
+   * Before connection (just for faye)
+   * @param {client} client connection
+   */
+  beforeConnect : function (client) {
+    // Your logic
+    // By example
+    // client.setHeader('Authorization', 'OAuth abcd-1234');
+    // client.disable('websocket')
+  },
+
+  /**
+   * on socket io connect
+   * @param {client} client connection
+   * @param {done}   callback function(err) {}
+   */
+  onConnect: function (client, done) {
+    // Your logic
+    // client.subscribe('/test', function() {});
+    client.on('GETSYSTEMMESSAGE', function(data) {
+    });
+    client.on('PULLMESSAGE', function(data) {
+      // console.log('push data', data);
+    })
+    done()
+  },
+
+  /**
+   * send a message
+   * @param {client} client connection
+   * @param {done}   callback function(err) {}
+   */
+  sendMessage : function (client, done) {
+    // util_sleep.sleep(10);
+    client.emit('ping', { msg: 'test' }, function(data) {
+    });
+    done();
+  }
+};
